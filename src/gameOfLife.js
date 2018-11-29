e70d914d89f6f95ac68459897f5044321fce2c4a
Ankon <44019225+luciferankon@users.noreply.github.com>
2018-11-29 14:19:46 +0530

    implemented relative positions in world

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -14,9 +14,12 @@ const nextGeneration = function(currGeneration,bounds) {
   let grid = initCell(height,width);
   let isWithinBound = isWithin.bind(null,bounds.topLeft,bounds.bottomRight);
   currGeneration = currGeneration.filter(isWithinBound);
+  let makePositionRelativeWith = makePositionRelative.bind(null,bounds.topLeft);
+  currGeneration = currGeneration.map(makePositionRelativeWith);
   let world = generateInitialWorld(grid,currGeneration);
   let updatedWorld = updateState(world);
   let result = [];
+  let relativeTopLeft = bounds.topLeft.map( x => -x);
   for(let i in updatedWorld){
     for(let j in updatedWorld[i]){
       if(updatedWorld[i][j]==1){
@@ -24,8 +27,11 @@ const nextGeneration = function(currGeneration,bounds) {
       }
     }
   }
-  return result;
+  return result.map(makePositionRelative.bind(null,relativeTopLeft));
 }
 
+const makePositionRelative = function(topLeft,position){
+  return [position[0]-topLeft[0],position[1]-topLeft[1]];
+}
 
 module.exports = { nextGeneration };
