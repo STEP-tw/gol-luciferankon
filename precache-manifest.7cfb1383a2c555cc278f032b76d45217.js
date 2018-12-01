0073cc4af4e871e15ef061b5a46f3027502adbaf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-01 06:59:34 +0000

    Deploy STEP-tw/gol-luciferankon to github.com/STEP-tw/gol-luciferankon.git:gh-pages

---
+++ precache-manifest.7cfb1383a2c555cc278f032b76d45217.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c161e6183cde81e1a57",
+    "url": "/gol-luciferankon/static/js/runtime~main.2c161e61.js"
+  },
+  {
+    "revision": "0b836f281134f0b6dedf",
+    "url": "/gol-luciferankon/static/js/main.0b836f28.chunk.js"
+  },
+  {
+    "revision": "43a0a152d6674d47e1c6",
+    "url": "/gol-luciferankon/static/js/1.43a0a152.chunk.js"
+  },
+  {
+    "revision": "0b836f281134f0b6dedf",
+    "url": "/gol-luciferankon/static/css/main.e8630a50.chunk.css"
+  },
+  {
+    "revision": "4224a292bcb0af42aad18a9a1f3541fb",
+    "url": "/gol-luciferankon/index.html"
+  }
+];
